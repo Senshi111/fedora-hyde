84de0644ad73fe4bec71334018f9875b5609e7cb
Bane <40372242+Senshi111@users.noreply.github.com>
2025-01-26 16:35:35 +0100

    Update themepatcher.sh

--- Scripts/themepatcher.sh
+++ Scripts/themepatcher.sh
@@ -262,11 +262,8 @@ done <<<"${wallpapers}"
 echo -en "${restore_list}" >"${Theme_Dir}/restore_cfg.lst"
 print_prompt -g "\n[exec] " "restore_cfg.sh \"${Theme_Dir}/restore_cfg.lst\" \"${Theme_Dir}/Configs\" \"${Fav_Theme}\"\n"
 "${scrDir}/restore_cfg.sh" "${Theme_Dir}/restore_cfg.lst" "${Theme_Dir}/Configs" "${Fav_Theme}" &>/dev/null
-if [ "${3}" != "--skipcaching" ]; then
-    "$HOME/.local/lib/hyde/swwwallcache.sh" -t "${Fav_Theme}"
-    "$HOME/.local/lib/hyde/themeswitch.sh"
-fi
+[ "${3}" == "--skipcaching" ] || "$HOME/.local/lib/hyde/swwwallcache.sh" -t "${Fav_Theme}"
 
-print_prompt -y "\nNote: Warnings are not errors. Review the output to check if it concerns you."
+print_prompt -y "\nNote: Warnings are not errors. Please review the output above for any warnings."
 
 exit 0
